6d5191e67c9e2063f8b222e16feecad848f8dc48
Aditiya <aditiyaart23@gmail.com>
2024-09-09 12:08:50 +1200

    updated handler for ffast

--- link_handler/handler.py
+++ link_handler/handler.py
@@ -9,17 +9,12 @@ class FuckingFast():
         response = requests.get(self._url)
         for line in response.text.split("\n"):
             if self._substring in line:
-                print(line.split("\"")[1])
+                return line.split("\"")[1]
 
-    def write(self) -> None:
-        # TODO
-        # implement writing to file capability
-        pass
-
-class DataNodes(FuckingFast):
+class Datanodes(FuckingFast):
     def __init__(self, url: str) -> None:
         super().__init__(url)
-        self._substring: str = "https://datanodes.io/"
+        self._substring: str = "https://datanodes.to/"
     
     def crawl(self) -> None:
-        print(f"{self._url}")
+        return self._url
